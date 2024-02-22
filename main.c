c49c1c26189a69d7a02031f1de0a12aa9eb6b3e2
celiaf525 <celia.f2000@gmail.com>
2024-02-21 18:01:48 -0800

    added args and socket/list initialization

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
+#include "list.h"
+#include "sockets.h"
+
 int main(int argc, char** args)
 {
+    // Initialize arguments
+    int myPort = args[1];
+    char *remoteMachine = args[2];
+    int remotePort = args[3];
+
+    // Establish socket connection
+    int socketDescriptor = createSocket(myPort);
+
+    // Create list variables
+    List *listRx = List_create(); // Storing inbound messages
+    List *listSx = List_create(); // Storing outbound messages
+
     // Intialize pthreads
-    input_init();
-    send_init();
-    receive_init();
-    print_init();
+    input_init(listSx);
+    send_init(listSx);
+    receive_init(listRx, socketDescriptor);
+    print_init(listRx);
 
     // Cleanup pthreads
     input_waitForShutdown();
     send_waitForShutdown();
     receive_waitForShutdown();
     print_waitForShutdown();
+
+    // Close socket
+    closeSocket(socketDescriptor);
 
     return 0;
 }
