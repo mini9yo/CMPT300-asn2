e9ffe4a275e34b3b2c33621e3d9c987cefe49b25
celiaf525 <celia.f2000@gmail.com>
2024-02-27 23:09:12 -0800

    removed variable freeing

--- receiveThread.c
+++ receiveThread.c
@@ -81,10 +81,4 @@ void receive_shutdown()
 {
     pthread_cancel(threadReceive);
     pthread_join(threadReceive, NULL);
-
-    // Free allocated memory
-    if (s_msgRx_allocated) {
-        free(s_msgRx_allocated);
-        s_msgRx_allocated = NULL;
-    }
 }
