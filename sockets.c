b3d84d6749da0fc8b548ae5c73a090ac161d97c9
kayleesryu <135180069+mini9yo@users.noreply.github.com>
2024-02-27 19:56:56 -0800

    Update sockets.c

--- sockets.c
+++ sockets.c
@@ -5,6 +5,8 @@
 #include "sockets.h"
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
 
  int createSocket(int PORT)
  {
@@ -22,8 +24,10 @@
         exit(EXIT_FAILURE);
     }
 
+
+
     // Bind the socket to the port
-    if (!bind (socketDescriptor, (struct sockaddr*) &sin, sizeof(sin))) {
+    if (bind(socketDescriptor, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
         perror("Error bind");
         exit(EXIT_FAILURE);
     } 
