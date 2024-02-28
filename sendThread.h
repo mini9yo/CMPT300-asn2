a2852505d954c1068115b8fbb61cc3b180d60620
kayleesryu <135180069+mini9yo@users.noreply.github.com>
2024-02-27 20:25:29 -0800

    signal sendThread.h

--- sendThread.h
+++ sendThread.h
@@ -2,13 +2,15 @@
 #define _SEND_H
 
 #include "list.h"
-
+static pthread_mutex_t s_sendMutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Send thread implementation
 void* sendThread(void* threadArgs);
 
 // Initialize sendThread
 void send_init(List* messageListSend, int socketDescriptor, char * remoteMachine, int remotePort);
+
+void send_signal();
 
 // Shutdown sendThread
 void send_shutdown();
