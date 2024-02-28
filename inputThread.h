d8c8873f35cf77b09b4745fa0b969e51d5963d78
sra133 <sra133@asb9838nu-e09.csil.sfu.ca>
2024-02-27 19:25:55 -0800

    added list.h in input/send threads header files

--- inputThread.h
+++ inputThread.h
@@ -1,11 +1,13 @@
 #ifndef _INPUT_H
 #define _INPUT_H
 
+#include "list.h"
+
 // Input thread implementation
 void* inputThread();
 
 // Initialize inputThread
-void input_init();
+void input_init(List* list);
 
 // Shutdown inputThread
 void input_waitForShutdown();
